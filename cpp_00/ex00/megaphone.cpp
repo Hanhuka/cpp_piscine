0f807e19f38657261addf0c6244c3212a89ea0fd
Ricardo Alves gil lopes <ralves-g@c1r20s4.42lisboa.com>
2023-01-06 18:42:48 +0000

    error on ex00... xD

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ralves-g <ralves-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:58:12 by ralves-g          #+#    #+#             */
-/*   Updated: 2022/11/24 15:19:38 by ralves-g         ###   ########.fr       */
+/*   Updated: 2023/01/06 17:55:47 by ralves-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	main(int ac, char **av)
 	{
 		for (int x = 0; av[y][x]; x++)
 			std::cout << (char)std::toupper(av[y][x]);
-		std::cout << " ";
 	}
 	std::cout << std::endl;
 }
