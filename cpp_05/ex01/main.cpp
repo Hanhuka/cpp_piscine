f1aed8ef8f3ac712a248bb0be616e6bf8dbf2123
Ricardo Alves gil lopes <ralves-g@c1r20s5.42lisboa.com>
2023-05-12 17:12:08 +0100

    added tests to main on cpp_05/ex01

--- cpp_05/ex01/main.cpp
+++ cpp_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ralves-g <ralves-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 16:58:35 by ralves-g          #+#    #+#             */
-/*   Updated: 2023/05/12 17:00:00 by ralves-g         ###   ########.fr       */
+/*   Updated: 2023/05/12 17:10:31 by ralves-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 # include "Form.hpp"
 
 int main() {
-	Bureaucrat hey("someDude");
+	Bureaucrat hey("someDude", 50);
 
-	Form aForm();
+	Form form1("justAForm", 70, 50);
+	Form form2("yetAnother", 60, 40);
+	Form form3("aMoreRigurousForm", 20, 20);
+
+	try
+	{
+		hey.signForm(form1);
+		hey.signForm(form2);
+		hey.signForm(form3);
+	}
+	catch (std::exception& e)
+	{
+		e.what();
+	}
 }
