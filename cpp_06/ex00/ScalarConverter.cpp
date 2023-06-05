0c34b98a9d7ec8ef16f057dc8214e5f7e5930973
Ricardo Alves gil lopes <ralves-g@c1r18s4.42lisboa.com>
2023-06-05 15:01:31 +0100

    ayyy

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ralves-g <ralves-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 17:19:14 by ralves-g          #+#    #+#             */
-/*   Updated: 2023/06/05 14:37:40 by ralves-g         ###   ########.fr       */
+/*   Updated: 2023/06/05 15:00:28 by ralves-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,18 @@ ScalarConverter&	ScalarConverter::operator=(ScalarConverter const& ref) {
 void	ScalarConverter::converter(std::string const& toConvert) {
 	int type = getType(toConvert);
 
-	std::cout << "type: " << type << std::endl;
-	switch (type) 
+	switch (type)
 	{
 		case 0:
 			castChr(toConvert);
 			break ;
-		case 1: //int
+		case 1:
 			castInt(toConvert);
 			break ;
 		case 2:
 			castFlt(toConvert);
 			break ;
-		case 3: //double
+		case 3:
 			castDbl(toConvert);
 			break ;
 		default :
