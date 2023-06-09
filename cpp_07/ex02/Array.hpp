149930bea7a94baf972d0eb20d85a21a5f242466
Ricardo Alves gil lopes <ralves-g@c1r20s4.42lisboa.com>
2023-06-09 18:25:25 +0100

    cpp07 done!!!!

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ralves-g <ralves-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:41:25 by ralves-g          #+#    #+#             */
-/*   Updated: 2023/06/09 14:21:07 by ralves-g         ###   ########.fr       */
+/*   Updated: 2023/06/09 18:11:11 by ralves-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,26 @@ template< typename T >
 class Array {
 	public:
 		Array(): _size(0), _array(new T[0]) {};
+		Array<unsigned int>(unsigned int const& size): _array(new T[size]), _size(size) {}
 		Array& operator=(Array const& ref) {
-			_size = ref._size;
 			if (_array)
-				delete _array;
+				delete [] _array;
+			_size = ref._size;
 			_array = new T[_size];
-			for (int i = 0; i < size; i++)
+			for (unsigned int i = 0; i < _size; i++)
 				_array[i] = ref._array[i];
+			return (*this);
 		}
-		Array(Array const& ref): _size(ref._size), _array(new T[ref._size]) {
+		Array(Array const& ref): _array(new T[ref._size]), _size(ref._size) {
 			*this = ref;
 		}
-		class OOBEXCEPTION : std::exception {
-			virtual const char *what() const _GLIBCXX_NOTHROW;
-		}
-		Array& operator[](int const& i) {
-
-			if (i < 0 || i >= _size)
+		class OOBEXCEPTION: public std::exception {
+			virtual const char *what() const _GLIBCXX_NOTHROW {
+				return ("Error: Out of bounds");
+			}
+		};
+		T& operator[](unsigned int const& i) {
+			if (i >= _size)
 				throw (Array::OOBEXCEPTION());
 			return (_array[i]);
 		}
@@ -45,11 +48,11 @@ class Array {
 		}
 		~Array()
 		{
-			delete _array;
+			delete [] _array;
 		}
 	private:
-		T *_array;
-		int _size;
+		T				*_array;
+		unsigned int	_size;
 };
 
 #endif
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ralves-g <ralves-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:21:11 by ralves-g          #+#    #+#             */
-/*   Updated: 2023/06/09 14:21:33 by ralves-g         ###   ########.fr       */
+/*   Updated: 2023/06/09 18:21:44 by ralves-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,57 @@
 # include "Array.hpp"
 
 int main() {
-
-
+	{
+		Array<unsigned int> test(0);
+	
+		std::cout << "test has a size of " << test.size() << std::endl;
+		try {
+			test[0] = 10;
+			std::cout << test[0] << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << "\n";
+	{
+		Array<std::string> test(6);
+	
+		std::cout << "test has a size of " << test.size() << std::endl;
+		try {
+			test[0] = "ola";
+			test[1] = "alo";
+			test[2] = "hi";
+			test[3] = "hey";
+			test[4] = "wassup";
+			test[5] = "yo";
+			for (int i = 0; i < test.size(); i++)
+				std::cout << "test[" << i << "] = " << test[i] << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << "\n";
+		Array<std::string> test2(0);
+		try {
+			test2 = test;
+			for (int i = 0; i < test2.size(); i++)
+				std::cout << "test2[" << i << "] = " << test2[i] << std::endl;
+		}
+		catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		std::cout << "\n";
+		Array<std::string> test3(test2);
+		try {
+			for (int i = 0; i < test3.size(); i++)
+				std::cout << "test3[" << i << "] = " << test3[i] << std::endl;
+		}
+		catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		
+	}
 }
